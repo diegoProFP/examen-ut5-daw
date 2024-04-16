3ec1e743567595f9c400d9ceb53866b43d047a10
AaronAlonsoAparicio <aaron.alonsoaparicio@educa.madrid.org>
2024-04-16 10:05:16 +0200

    Añadidos comentarios en JavaDoc

--- SpotifyExamen/src/spotify/aaronalonsoaparicio/Playlist.java
+++ SpotifyExamen/src/spotify/aaronalonsoaparicio/Playlist.java
@@ -7,6 +7,14 @@ import java.util.List;
 //De autor poned vuestro email de educaMadrid. 
 //La version de la clase es la 1.5
 
+/**
+ * Clase  que nos define las caracteristicas y las funciones de la Playlist
+ *@autor aaronalonsoaparicio
+ *@version 1.5 
+ *
+ */
+
+
 public class Playlist {
 
 	private String nombre;
@@ -16,6 +24,12 @@ public class Playlist {
 	
 	// Poner descripcion, valor de retorno.
 	//En los comentarios describir cómo se obtienen los minutos y a partir de qué dato de la clase.
+	
+	/**
+	 * Este metodo nos dice en segundos cuanto dura la Playlist, obtenemos el valor de los valor totalSegundos de la clase
+	 * 
+	 * @return totalSegundos lo pasamos a minutos y nos devuelve el valor con decimales 
+	 */
 	public double obtenerDuracionTotal() {
 		return (double) totalSegundos / 60;
 	}
@@ -25,6 +39,17 @@ public class Playlist {
 	// Poner descripcion, parametros de entrada, valor de retorno, y excepción. Además que pueda
 	// referenciar tanto a las clases Cancion y DatosInvalidosException
 	//Existe desde la version 1.0
+	
+	/**
+	 * Agragamos una nueva cancion la Playlist
+	 * @param nueva
+	 * @return exito true si la cancion no esta en la Playlist false si si esta.
+	 * @throws DatosInvalidosException Lanzamos una excepcion cuando no añadimos una cancion o el nombre de esta esta vacio
+	 * @since 1.0
+	 * @see #Cancion
+	 * @see #DatosInvalidosException
+	 */
+	
 	public boolean agregarCancion(Cancion nueva) throws DatosInvalidosException {
 
 		boolean exito = false;
@@ -47,6 +72,13 @@ public class Playlist {
 	
 	// Poner descripcion, parametros de entrada, valor de retorno.
 	//Existe desde la version 1.5
+	
+	/**
+	 * Bucamos una cancion segun su nombre
+	 * @param nombre
+	 * @return encontrado true si se ha encontrado, false si no.
+	 * @since 1.5
+	 */
 	public boolean encontrarCancionPorNombre(String nombre) {
 		boolean encontrado = false;
 		int contador = 0;
@@ -67,6 +99,15 @@ public class Playlist {
 	// Este metodo está deprecado porque se ha hecho uno mejor, y el que lo sustituye es el
 	// encontrarCancionPorNombre(String nombre)
 	//Existe desde la version 1.0
+	
+	
+	/**
+	 * Buscamos una cancion en la Playlist
+	 * @param nombreCancion
+	 * @return encontrado
+	 * @deprecated Este metodo ya no se usa, se ha sustituido por {@link #encontrarCancionPorNombre(String)}
+	 * @since  1.0
+	 */
 	public boolean encontrarCancion(String nombreCancion) {
 		boolean encontrado = false;
 
