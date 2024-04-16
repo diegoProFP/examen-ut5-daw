47d8be91f6b57fd0bd7c752457fbae429b878151
Saylarx <meginopovedalex@gmail.com>
2024-04-16 10:21:08 +0200

    Hago comentarios JavaDoc

--- SpotifyExamen/src/spotify/luismegino/Playlist.java
+++ SpotifyExamen/src/spotify/luismegino/Playlist.java
@@ -7,24 +7,50 @@ import java.util.List;
 //De autor poned vuestro email de educaMadrid. 
 //La version de la clase es la 1.5
 
+/**
+ * Esta clase sirve para que se invoque en un main usandose para crear listas de
+ * canciones y con los metodos que esta tiene agregar o encontrar canciones.
+ * 
+ * @author luis.megino@educamdrid.org
+ * @version 1.5
+ */
+
 public class Playlist {
 
 	private String nombre;
 	private int totalSegundos;
 	private List<Cancion> canciones;
 
-	
 	// Poner descripcion, valor de retorno.
-	//En los comentarios describir cómo se obtienen los minutos y a partir de qué dato de la clase.
+	// En los comentarios describir cómo se obtienen los minutos y a partir de qué
+	// dato de la clase.
+
+	/**
+	 * Este metodo obvtiene los segundos que dura una cancion y los divide entre 60
+	 * para pasarlo a minutos.
+	 * 
+	 * @return Devuelve el valor total de los segundos dividido entre 60.
+	 */
 	public double obtenerDuracionTotal() {
 		return (double) totalSegundos / 60;
 	}
-		
-		
-		
-	// Poner descripcion, parametros de entrada, valor de retorno, y excepción. Además que pueda
+
+	// Poner descripcion, parametros de entrada, valor de retorno, y excepción.
+	// Además que pueda
 	// referenciar tanto a las clases Cancion y DatosInvalidosException
-	//Existe desde la version 1.0
+	// Existe desde la version 1.0
+
+	/**
+	 * Este metodo agrega una cancion {@link Cancion} a la lista de canciones
+	 * obteniendo el objeto cancion.
+	 * 
+	 * @param nueva Es un objeto de la clase cancion usado para agregar esta a la
+	 *              lista.
+	 * @throws DatosInvalidosException Lanza esta excepcion en caso de que los datos
+	 *                                 de la cancion no sean validos.
+	 *                                 {@link DatosInvalidosException}
+	 * @since 1.0
+	 */
 	public boolean agregarCancion(Cancion nueva) throws DatosInvalidosException {
 
 		boolean exito = false;
@@ -43,10 +69,17 @@ public class Playlist {
 		return exito;
 	}
 
-
-	
 	// Poner descripcion, parametros de entrada, valor de retorno.
-	//Existe desde la version 1.5
+	// Existe desde la version 1.5
+
+	/**
+	 * Este metodo al introducirle un nombre busca en la lista de canciones si
+	 * alguna coincide con ese nombre, si es así devuelve true.
+	 * 
+	 * @param nombre Este es el nombre introducido y es usado para buscar en la
+	 *               lista si coincide con alguna cancion.
+	 * @since 1.5
+	 */
 	public boolean encontrarCancionPorNombre(String nombre) {
 		boolean encontrado = false;
 		int contador = 0;
@@ -63,10 +96,20 @@ public class Playlist {
 
 		return encontrado;
 	}
-	
-	// Este metodo está deprecado porque se ha hecho uno mejor, y el que lo sustituye es el
+
+	// Este metodo está deprecado porque se ha hecho uno mejor, y el que lo
+	// sustituye es el
 	// encontrarCancionPorNombre(String nombre)
-	//Existe desde la version 1.0
+	// Existe desde la version 1.0
+
+	/**
+	 * Metodo para encontrar canciones obsoleto.
+	 * 
+	 * @deprecated Este metodo esta obsoleto, se mejora en el metodo:
+	 *             encontrarCancionPorNombre
+	 * @see encontrarCancionPorNombre(String)
+	 * @since 1.0
+	 */
 	public boolean encontrarCancion(String nombreCancion) {
 		boolean encontrado = false;
 
@@ -76,12 +119,11 @@ public class Playlist {
 				encontrado = true;
 				break;
 			}
-			
+
 		}
 		return encontrado;
 	}
 
-	
 	public String getNombre() {
 		return nombre;
 	}
