206363df69ddb222d57ff716acd0139c706cf532
34633 <34633@DESKTOP-RSD6JVT>
2024-04-16 10:34:15 +0200

    segundo commit

--- SpotifyExamen/src/spotify/idel2/Playlist.java
+++ SpotifyExamen/src/spotify/idel2/Playlist.java
@@ -3,106 +3,109 @@ package spotify.idel2;
 import java.util.ArrayList;
 import java.util.List;
 
-//Poner comentario de la clase, con la descripción de qué es lo que hace  
-//De autor poned vuestro email de educaMadrid. 
-//La version de la clase es la 1.5
-
+/**
+ * Clase que representa una lista de reproducción de canciones.
+ * Permite gestionar el nombre de la lista, la duración total en minutos
+ * y las canciones que contiene.
+ * Autor: tuemail@educaMadrid
+ * Version: 1.5
+ */
 public class Playlist {
 
-	private String nombre;
-	private int totalSegundos;
-	private List<Cancion> canciones;
+    private String nombre;
+    private int totalSegundos;
+    private List<Cancion> canciones;
 
-	
-	// Poner descripcion, valor de retorno.
-	//En los comentarios describir cómo se obtienen los minutos y a partir de qué dato de la clase.
-	public double obtenerDuracionTotal() {
-		return (double) totalSegundos / 60;
-	}
-		
-		
-		
-	// Poner descripcion, parametros de entrada, valor de retorno, y excepción. Además que pueda
-	// referenciar tanto a las clases Cancion y DatosInvalidosException
-	//Existe desde la version 1.0
-	public boolean agregarCancion(Cancion nueva) throws DatosInvalidosException {
+    /**
+     * Calcula la duración total de la lista de reproducción en minutos.
+     * La duración se obtiene dividiendo el total de segundos entre 60.
+     *
+     * @return La duración total en minutos.
+     */
+    public double obtenerDuracionTotal() {
+        return (double) totalSegundos / 60;
+    }
 
-		boolean exito = false;
-		if (canciones == null) {
-			this.canciones = new ArrayList<>();
-		}
-		if (nueva == null || nueva.getNombre() == null) {
-			throw new DatosInvalidosException("La cancion o su nombre es vacía");
-		}
+    /**
+     * Agrega una nueva canción a la lista de reproducción.
+     *
+     * @param nueva La canción que se va a agregar.
+     * @return true si la canción se agrega con éxito, false si la canción ya existe en la lista.
+     * @throws DatosInvalidosException si la canción o su nombre es nulo.
+     */
+    public boolean agregarCancion(Cancion nueva) throws DatosInvalidosException {
+        boolean exito = false;
+        if (canciones == null) {
+            this.canciones = new ArrayList<>();
+        }
+        if (nueva == null || nueva.getNombre() == null) {
+            throw new DatosInvalidosException("La canción o su nombre es vacío");
+        }
+        boolean existeCancion = this.encontrarCancionPorNombre(nueva.getNombre());
+        if (!existeCancion) {
+            exito = canciones.add(nueva);
+        }
+        return exito;
+    }
 
-		boolean existeCancion = this.encontrarCancionPorNombre(nueva.getNombre());
-		if (!existeCancion) {
-			exito = canciones.add(nueva);
-		}
+    /**
+     * Busca una canción en la lista de reproducción por su nombre.
+     *
+     * @param nombre El nombre de la canción que se desea buscar.
+     * @return true si la canción se encuentra en la lista, false en caso contrario.
+     */
+    public boolean encontrarCancionPorNombre(String nombre) {
+        boolean encontrado = false;
+        int contador = 0;
+        while (!encontrado && contador < canciones.size()) {
+            if (canciones.get(contador).getNombre().equalsIgnoreCase(nombre)) {
+                encontrado = true;
+            }
+            contador++;
+        }
+        return encontrado;
+    }
 
-		return exito;
-	}
+    /**
+     * Este método está deprecado porque se ha implementado uno mejor: encontrarCancionPorNombre(String nombre).
+     * @param nombreCancion El nombre de la canción que se desea buscar.
+     * @return true si la canción se encuentra en la lista, false en caso contrario.
+     * @deprecated Este método está deprecado y se recomienda utilizar encontrarCancionPorNombre(String nombre).
+     */
+    @Deprecated
+    public boolean encontrarCancion(String nombreCancion) {
+        boolean encontrado = false;
+        for (int cont = 0; cont < canciones.size(); cont++) {
+            Cancion cancionExtraida = this.canciones.get(cont);
+            if (cancionExtraida.getNombre().equals(nombreCancion)) {
+                encontrado = true;
+                break;
+            }
+        }
+        return encontrado;
+    }
 
+    public String getNombre() {
+        return nombre;
+    }
 
-	
-	// Poner descripcion, parametros de entrada, valor de retorno.
-	//Existe desde la version 1.5
-	public boolean encontrarCancionPorNombre(String nombre) {
-		boolean encontrado = false;
-		int contador = 0;
-		while (!encontrado && contador < canciones.size()) {
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 
-			// Si el nombre de la cancion obtenida en cada vuelta es igual a la que se
-			// quiere
-			if (canciones.get(contador).getNombre().equalsIgnoreCase(nombre)) {
-				encontrado = true;
-			}
+    public int getTotalSegundos() {
+        return totalSegundos;
+    }
 
-			contador++;
-		}
+    public void setTotalSegundos(int totalSegundos) {
+        this.totalSegundos = totalSegundos;
+    }
 
-		return encontrado;
-	}
-	
-	// Este metodo está deprecado porque se ha hecho uno mejor, y el que lo sustituye es el
-	// encontrarCancionPorNombre(String nombre)
-	//Existe desde la version 1.0
-	public boolean encontrarCancion(String nombreCancion) {
-		boolean encontrado = false;
+    public List<Cancion> getCanciones() {
+        return canciones;
+    }
 
-		for (int cont = 0; cont < canciones.size(); cont++) {
-			Cancion cancionExtraida = this.canciones.get(cont);
-			if (cancionExtraida.getNombre().equals(nombreCancion)) {
-				encontrado = true;
-				break;
-			}
-			
-		}
-		return encontrado;
-	}
-
-	
-	public String getNombre() {
-		return nombre;
-	}
-
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-
-	public int getTotalSegundos() {
-		return totalSegundos;
-	}
-
-	public void setTotalSegundos(int totalSegundos) {
-		this.totalSegundos = totalSegundos;
-	}
-
-	public List<Cancion> getCanciones() {
-		return canciones;
-	}
-
-	public void setCanciones(List<Cancion> canciones) {
-		this.canciones = canciones;
-	}
+    public void setCanciones(List<Cancion> canciones) {
+        this.canciones = canciones;
+    }
 }
