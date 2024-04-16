47ad82d18f991c5d5d0cdef988c2a290c57281a4
aymann78 <aymanassidah78@gmail.com>
2024-04-16 10:18:56 +0200

    Creación de paquete spotify.aymanassidah

---
+++ SpotifyExamen/src/spotify/aymanassidah/OperacionNoPermitidaException.java
@@ -0,0 +1,10 @@
+package spotify.aymanassidah;
+
+public class OperacionNoPermitidaException extends Exception {
+
+	public OperacionNoPermitidaException(String mensaje) {
+		super(mensaje);
+	}
+	
+
+}
