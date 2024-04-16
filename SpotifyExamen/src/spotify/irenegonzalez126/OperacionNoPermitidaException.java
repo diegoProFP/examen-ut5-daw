6f3d212cda1d62c53aaebc8243b419679841500b
irene <irenegonro2@gmail.com>
2024-04-16 10:19:27 +0200

    Añadidas las clases al nuevo paquete spotify.irenegonzalez126

---
+++ SpotifyExamen/src/spotify/irenegonzalez126/OperacionNoPermitidaException.java
@@ -0,0 +1,10 @@
+package spotify.irenegonzalez126;
+
+public class OperacionNoPermitidaException extends Exception {
+
+	public OperacionNoPermitidaException(String mensaje) {
+		super(mensaje);
+	}
+	
+
+}
